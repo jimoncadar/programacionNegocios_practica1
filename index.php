ed3d9c8f29a96ba0834490c10471fabe32856f53
jimoncadar <jimoncadar@unicah.edu>
2025-03-26 18:18:52 -0600

    Estableciendo conexión con la bdd.

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
     $error="";
     $hay_post = false;
-
+    $nombre = "";
+    $sexo = "";
+    $pais = "";
     if(isset($_REQUEST['submit1'])){
         $hay_post = true;
         $nombre = isset($_REQUEST['txtNombre']) ? $_REQUEST['txtNombre'] : "";
         $sexo = isset($_REQUEST['radioSexo']) ? $_REQUEST['radioSexo'] : "";
-
+        
         $pais = isset($_REQUEST['cmbPais']) ? $_REQUEST['cmbPais'] : "";
+        
 
         if(!empty($nombre)){
             $nombre = preg_replace("/[^a-zA-ZáéíóúÁÉÍÓÚ]/u","",$nombre);
@@ -25,32 +28,50 @@
         }
         
     }
+
+    $conexion = new PDO(
+        "mysql:dbname=programacionnegocios;127.0.0.1,",
+        "root",
+        "123456789"    
+    );
+
+        
+
+    $sql = 'select * from cliente';
+    $resultado = $conexion->query($sql);
+    foreach($resultado as $registro){
+        print($registro['nombreUsuario']);
+        print($registro['sexo']);
+        print($registro['pais']);
+    }
+
+    
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CRUD</title>
 </head>
 <body>
-    <form action="index.php" method="post">
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
         <label for="nombre">Nombre Completo:</label>
-        <input type="text" name="txtNombre" id="nombre"><br>
+        <input type="text" name="txtNombre" id="nombre" value="<?php echo isset($nombre)? $nombre : "" ?>"><br>
 
         <label for="hombre">Hombre</label>
-        <input type="radio" name="radioSexo" id="hombre" value="Hombre">
+        <input type="radio" name="radioSexo" id="hombre" value="Hombre" <?php  if($sexo=='Hombre'){echo "checked";} ?> >
 
         <label for="mujer">Mujer</label>
-        <input type="radio" name="radioSexo" id="mujer" value="Mujer"><br>
+        <input type="radio" name="radioSexo" id="mujer" value="Mujer" <?php if($sexo=='Mujer'){ echo "checked"; } ?> ><br>
         
         <label for="pais">País</label>
         <select name="cmbPais" id="pais">
             <option value="">Seleccione un país</option>
-            <option value="Honduras">Honduras</option>
-            <option value="Guatemala">Guatemala</option>
-            <option value="Mexico">Mexico</option>
+            <option value="Honduras" <?php echo ($pais=='Honduras')? 'selected' : '' ?> >Honduras</option>
+            <option value="Guatemala" <?php echo ($pais=='Guatemala')? 'selected' : '' ?>>Guatemala</option>
+            <option value="Mexico" <?php echo  ($pais=='Mexico')? 'selected' : '' ?>>Mexico</option>
         </select><br>
         <input type="submit" value="Enviar" name="submit1">
     </form>
@@ -64,5 +85,19 @@
             echo "País:$pais";
         }
     ?>
+
+
+        <table border="1">
+            <thead>
+                <th>Nombre</th>
+                <th>Sexo</th>
+                <th>País</th>
+            </thead>
+            <tbody>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tbody>
+        </table>
 </body>
 </html>
